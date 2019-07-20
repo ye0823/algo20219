4f9af84495861a9cdbbe0247dcaa1983b141d997
ye0823 <ye880823@gmail.com>
2019-07-20 15:25:15 +0900

    init

---
+++ rule/driving_client.py
@@ -0,0 +1,77 @@
+from drive_controller import DrivingController
+
+
+class DrivingClient(DrivingController):
+    def __init__(self):
+        # =========================================================== #
+        #  Area for member variables =============================== #
+        # =========================================================== #
+        # Editing area starts from here
+        #
+
+        self.is_debug = True
+        self.collision_flag = True
+
+        #
+        # Editing area ends
+        # ==========================================================#
+        super().__init__()
+
+    def control_driving(self, car_controls, sensing_info):
+        print(sensing_info)
+        print(car_controls)
+
+        # =========================================================== #
+        # Area for writing code about driving rule ================= #
+        # =========================================================== #
+        # Editing area starts from here
+        #
+
+        if self.is_debug:
+            print("=========================================================")
+            print("to middle: {}".format(sensing_info.to_middle))
+
+            print("collided: {}".format(sensing_info.collided))
+            print("car speed: {} km/h".format(sensing_info.speed))
+
+            print("is moving forward: {}".format(sensing_info.moving_forward))
+            print("moving angle: {}".format(sensing_info.moving_angle))
+            print("lap_progress: {}".format(sensing_info.lap_progress))
+
+            print("track_forward_angles: {}".format(sensing_info.track_forward_angles))
+            print("track_forward_obstacles: {}".format(sensing_info.track_forward_obstacles))
+            print("opponent_cars_info: {}".format(sensing_info.opponent_cars_info))
+            print("distance_to_way_points: {}".format(sensing_info.distance_to_way_points))
+            print("=========================================================")
+
+        ###########################################################################
+
+        # Moving straight forward
+        car_controls.steering = 0
+        car_controls.throttle = 1
+        car_controls.brake = 0
+
+        if self.is_debug:
+            print("steering:{}, throttle:{}, brake:{}".format(car_controls.steering, car_controls.throttle, car_controls.brake))
+
+        #
+        # Editing area ends
+        # ==========================================================#
+        return car_controls
+
+
+    # ============================
+    # If you have NOT changed the <settings.json> file
+    # ===> player_name = ""
+    #
+    # If you changed the <settings.json> file
+    # ===> player_name = "My car name" (specified in the json file)  ex) Car1
+    # ============================
+    def set_player_name(self):
+        player_name = ""
+        return player_name
+
+
+if __name__ == '__main__':
+    client = DrivingClient()
+    client.run()
